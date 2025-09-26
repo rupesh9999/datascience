8f6aa43779a5238118a83709ca38f5d03eafe481
rupesh9999 <rupesh.b500@gmail.com>
2025-09-26 07:34:15 +0530

    created logging

---
+++ 12-Logging_In_Python/app.py
@@ -0,0 +1,46 @@
+import logging 
+
+## logging settings
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        logging.FileHandler("app.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logger=logging.getLogger("ArithmeticApp")
+
+def add(a,b):
+    result=a+b
+    logger.debug(f"Adding {a} + {b} = {result}")
+    return result 
+
+def sub(a,b):
+    result=a-b
+    logger.debug(f"Subtracting {a} - {b}", result)
+    return result
+
+def mul(a,b):
+    result=a*b
+    logger.debug(f"Multiplying {a} * {b}", result)
+    return result
+
+def div(a,b):
+   try:
+    result=a/b
+    logger.debug(f"Dividing {a} / {b}", result)
+    return result
+   except ZeroDivisionError:
+      logger.error("Division by zero error")
+      return None
+
+
+add(10,15)
+sub(10,15)
+mul(10,15)
+div(10,15)
+div(10,0)
